0e0a8fcc9c95b75ea797a7ec24105ea2c075aa74
Ben Burkert <ben@benburkert.com>
2009-04-20 17:55:13 -0500

    Simple app generation implemented.

--- lib/generators/application.rb
+++ lib/generators/application.rb
@@ -6,7 +6,22 @@ module SousChef
       Generates a new chef cookbook with recipes and nodes.
       DESC
 
-      first_argument :name, :required => true, :desc => "Application name"
+      first_argument :name, :required => true, :desc => "Application Name"
+
+      desc <<-DESC
+      The Target plateform the application for deployment.
+      DESC
+
+      option :platform, :default => :none, :desc => "Target Platform"
+
+      desc <<-DESC
+      An initial deployment node.
+      DESC
+
+      option :node, :default => :none, :desc => "Initial Node"
+
+      empty_directory :nodes, 'nodes'
+      empty_directory :recipes, 'recipes'
     end
 
     add :app, ApplicationGenerator
--- lib/sous-chef/generator.rb
+++ lib/sous-chef/generator.rb
@@ -2,7 +2,14 @@ module SousChef
   module Generators
     extend Templater::Manifold
 
+    desc <<-DESC
+      Generate cookbooks, nodes, and recipes for your chef scripts.
+    DESC
+
     class Generator < ::Templater::Generator
+      def destination_root
+        File.join(@destination_root, name)
+      end
     end
   end
 end
